0f8767aed6edaf4832c7fa5284849dc64d23a93a
Jekeee <glooffee@gmail.com>
2022-07-23 23:29:01 +0300

    s1

--- src/Todo Components/TodoItem.js
+++ src/Todo Components/TodoItem.js
@@ -1,14 +1,30 @@
 import React from "react";
 
+const styles = {
+    li : {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem'
+    },
+    input: {
+        marginRight: '1rem',
+    }
+}
+
 function TodoItem({todo, index}) {
     return(
-        <li>
+        <li style={styles.li}>
             <span>
-                <input type="checkbox"/>
+                <input style={styles.input} type="checkbox"/>
                 <strong>{index +1}</strong>
+                &nbsp;
                 {todo.title}
             </span>
-            <button>Удалить</button>
+            <button className="btnX">X</button>
         </li>
     )
 }
